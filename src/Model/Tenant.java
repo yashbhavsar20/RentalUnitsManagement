984c4451143a8849c55ae0617ed9a5e60bfe2bc6
manish198 <gautam.manish.manish@gmail.com>
2023-02-27 22:18:12 -0500

    Tentant class created with tenant arributes

--- src/Model/Tenant.java
+++ src/Model/Tenant.java
@@ -1,4 +1,12 @@
 package Model;
 
 public class Tenant {
+    private String name;
+    private long sin;
+
+    public Tenant(String name,long sin){
+        this.name=name;
+        this.sin=sin;
+    }
+
 }
