ad15704ad0ff3b1fd67db18b199d89cbbacf6434
manish198 <gautam.manish.manish@gmail.com>
2023-02-27 22:27:47 -0500

    Interrface method update

--- src/Interface/RentalSystemInterface.java
+++ src/Interface/RentalSystemInterface.java
@@ -1,13 +1,16 @@
 package Interface;
 
+import Model.Property.Property;
+import Model.Tenant.Tenant;
+
 public interface RentalSystemInterface {
 		void addProperty(String type,int streetNumber, String streetName, String cityName,
 						 String postalCode, String civicAddress, int apartmentNumber, int numberOfBedRooms,
 						 int numberOfBathRooms, double squareFoot);
 
-		void addTenant(String tenantName);
+		void addTenant(String tenantName,long sin);
 		
-		void rentUnit();
+		void rentUnit(Property property, Tenant tenant);
 		
 		String displayProperty();
 		
--- src/Model/Tenant/Tenant.java
+++ src/Model/Tenant/Tenant.java
@@ -1,4 +1,4 @@
-package Model;
+package Model.Tenant;
 
 public class Tenant {
     private String name;
