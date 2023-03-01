dc4950ce51c538ca9a826d307d6cb5443c9753ed
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-03-01 18:20:38 -0500

    Created Address class

---
+++ src/model/address/Address.java
@@ -0,0 +1,46 @@
+package model.address;
+
+public class Address {
+    private String postalCode;
+    private String province;
+    private String city;
+
+    public Address(String postalCode, String province, String city) {
+        this.postalCode = postalCode;
+        this.province = province;
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "postalCode='" + postalCode + '\'' +
+                ", province='" + province + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
