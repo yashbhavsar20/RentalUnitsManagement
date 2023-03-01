032c23c4944150f97eb1860a14ca4f049cf768f2
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-03-01 18:32:14 -0500

    updated apartmentBuilding model

--- src/model/property/concrete_property/ApartmentBuilding.java
+++ src/model/property/concrete_property/ApartmentBuilding.java
@@ -1,12 +1,41 @@
 package model.property.concrete_property;
 
+import model.address.Address;
 import model.property.Property;
 
-public class ApartmentBuilding extends Property {
-    private String civicAddress;
-    Apartment apartment;
+import java.util.HashMap;
 
-    public String addProperty(){
-       return "";
-    };
+public class ApartmentBuilding extends Property {
+    private Address address;
+    private String civicAddress;
+    private HashMap<Integer, PropertyDetails> apartments = new HashMap<Integer, PropertyDetails>();
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getCivicAddress() {
+        return civicAddress;
+    }
+
+    public void setCivicAddress(String civicAddress) {
+        this.civicAddress = civicAddress;
+    }
+
+    public HashMap<Integer, PropertyDetails> getApartments() {
+        return apartments;
+    }
+
+    public void setApartments(HashMap<Integer, PropertyDetails> apartments) {
+        this.apartments = apartments;
+    }
+
+    @Override
+    public String addProperty() {
+        return null;
+    }
 }
