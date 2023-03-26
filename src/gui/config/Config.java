52fa61693f08b35b3fa46be4d0429bd75bd754d1
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-03-26 15:52:31 -0400

    Updated view for property screens and sub screens

--- src/gui/config/Config.java
+++ src/gui/config/Config.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import view.RentalSystemAPI;
 
 
 public class Config extends Application implements Runnable {
--- src/gui/controller/PropertyScreenController.java
+++ src/gui/controller/PropertyScreenController.java
@@ -1,9 +1,14 @@
 package gui.controller;
 
 import gui.constants.Constant;
+import gui.controller.property.DisplayPropertyController;
 import gui.utils.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -16,7 +21,15 @@ public class PropertyScreenController extends SwitchScene {
         screenSwitch(Constant.ADD_PROPERTY, event);
     }
     public void onDisplayPropertyButton(ActionEvent event) throws IOException {
-        screenSwitch(Constant.DISPLAY_PROPERTY, event);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(Constant.DISPLAY_PROPERTY));
+        root = loader.load();
+
+        DisplayPropertyController displayPropertyController = loader.getController();
+
+        displayPropertyController.showProperties(Constant.api.getAllProperties());
+
+//        To switch screen using root whenever something has changed in view
+        screenSwitch(root, event);
     }
     public void onDisplayRentedUnitsButton(ActionEvent event) throws IOException {
         screenSwitch(Constant.DISPLAY_RENTED_UNITS, event);
