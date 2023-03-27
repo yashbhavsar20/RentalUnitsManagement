53357d6d272da10c4b29074b006e6040a6b6e2cb
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-03-27 15:17:22 -0400

    Updated display property page

--- src/gui/controller/property/DisplayPropertyController.java
+++ src/gui/controller/property/DisplayPropertyController.java
@@ -2,15 +2,16 @@ package gui.controller.property;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 
 public class DisplayPropertyController {
 
     @FXML
-    Label propertiesLabel;
+    ListView outputListView;
 
     @FXML
     public void showProperties(String properties) {
-        propertiesLabel.setText("Hello" + properties);
+        outputListView.getItems().add(properties);
     }
 }
