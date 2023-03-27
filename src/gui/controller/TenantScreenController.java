1e22f4ef4dd492dc3b9dfd5aa0d9252a0c44b781
manish198 <gautam.manish.manish@gmail.com>
2023-03-27 10:27:03 -0400

    Minor Update

--- src/gui/controller/TenantScreenController.java
+++ src/gui/controller/TenantScreenController.java
@@ -10,7 +10,12 @@ import java.io.IOException;
 public class TenantScreenController extends SwitchScene {
 
     @FXML
+    public void onAddTenantButton(ActionEvent event) throws IOException{
+        screenSwitch(Constant.ADD_TENANT,event);
+    }
+    @FXML
     public void onHomeClick(ActionEvent event) throws IOException {
         screenSwitch(Constant.HOME_SCREEN,event);
     }
+
 }
