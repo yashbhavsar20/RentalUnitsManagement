e276904769326c96a5843698be6258f1a889d24e
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-03-27 16:01:21 -0400

    Updated property components

--- src/gui/controller/property/MakePropertyAvailableController.java
+++ src/gui/controller/property/MakePropertyAvailableController.java
@@ -4,18 +4,26 @@ import gui.constants.Constant;
 import gui.utils.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class MakePropertyAvailableController extends SwitchScene {
 
     @FXML
     TextField propertyID;
+    @FXML
+    Label resLabel;
 
     @FXML
-    public void submit(){
+    public void submit(ActionEvent event) throws InterruptedException, IOException {
         String res = Constant.api.makePropertyAvailable(propertyID.getText());
+        resLabel.setText(res);
+        if(res.equals("Property is now available")){
+            propertyID.setText("");
+        }
     }
 
     @FXML
